15f222277bdfb503dcae877e06eb09e14e613f3a
Red-Rapious <red.rapious@gmail.com>
2022-06-16 21:52:27 +0200

    Updated token

--- bot_config.py
+++ bot_config.py
@@ -3,4 +3,4 @@ SERVEUR_ID = 882700982218268773 #id du serveur
 TD_GRP_1_ID = 882700982235062325 # TD grp 1
 ELEVES_ID  = 882700982247641172 # rôle élève
 GENERAL_SANS_PROFS_ID = 882700982574792758 # id du channel general_sans_profs
-TOKEN = 'ODk3NTAzMTUxNTQ3MDk3MTI4.YWWm8g.DO_7_7ey-U4kmTMvW43lRTPchYU'  # https://discord.com/developers/applications/
+TOKEN = "ODk3NTAzMTUxNTQ3MDk3MTI4.Gct6Kt.MoS1qCNCJvn44WBwp6PiVFHq0HqzTVlnuAM7dg"  # https://discord.com/developers/applications/
